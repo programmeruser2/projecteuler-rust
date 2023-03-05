4987dc4f4d9d2bb6b0697a6ba7c43abf40bd050b
programmeruser <programmeruser@programmeruser.tk>
2023-03-05 18:09:27 -0500

    problem 3

---
+++ 3.rs
@@ -0,0 +1,17 @@
+fn main() {
+	let mut maxfactor: u64 = 1; // 1 so that rustc doesn't complain
+	let mut n: u64 = 600851475143;
+	// Precision errors with large numbers, but that probably won't apply here
+	for i in 2..(n as f64).sqrt() as u64 {
+		if n % i == 0 {
+			maxfactor = i;
+			while n % i == 0{
+				n /= i;
+			}
+		}
+	}
+	if n > 1 {
+		maxfactor = n;
+	}
+	println!("{maxfactor}");
+}
